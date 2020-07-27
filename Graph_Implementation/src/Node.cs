2e5d53ac5baf0c235a4f3d874384fc261a8ae287
busrasenderin <bsndrn16@gmail.com>
2020-07-27 23:20:51 +0300

    Updated Node class

--- Graph_Implementation/src/Node.cs
+++ Graph_Implementation/src/Node.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Graph_Implementation
 {
     public class Node
     {
         public string name;
+        private List<Edge> connections;
 
         public Node(string name)
         {
             this.name = name;
+            connections = new List<Edge>();
+        }
+
+        public void AddConnection(Edge edge) {
+            connections.Add(edge);
+        }
+
+        public List<Edge> GetConnections() {
+            return connections;
         }
     }
 }
